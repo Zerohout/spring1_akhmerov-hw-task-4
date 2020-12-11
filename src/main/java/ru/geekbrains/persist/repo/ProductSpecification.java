49ba67057fb282a9f9b78401268d1eb52205873f
Zerohout <zerohout@gmail.com>
2020-12-11 12:17:41 +0300

    homework task 4 is done

--- src/main/java/ru/geekbrains/persist/repo/ProductSpecification.java
+++ src/main/java/ru/geekbrains/persist/repo/ProductSpecification.java
@@ -9,6 +9,14 @@ public class ProductSpecification {
         return (root, query, builder) -> builder.like(root.get("name"), "%" + name + "%");
     }
 
+    public static Specification<Product> minPrice(int minPrice){
+        return (root, query, builder) -> builder.greaterThan(root.get("price"),minPrice);
+    }
+
+    public static Specification<Product> maxPrice(int maxPrice){
+        return (root, query, builder) -> builder.lessThan(root.get("price"), maxPrice);
+    }
+
     public static Specification<Product> betweenPrice(int minPrice, int maxPrice){
         return (root, query, builder) -> builder.between(root.get("price"), minPrice, maxPrice);
     }
